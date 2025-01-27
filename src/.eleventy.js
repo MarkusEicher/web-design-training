d4a837870cf72ade6f7364f9bec2f5d102b2752f
MarkusEicher <markuseicher@outlook.com>
2025-01-27 01:09:46 +0100

    Reconfigure Eleventy

--- src/.eleventy.js
+++ src/.eleventy.js
@@ -2,7 +2,7 @@ module.exports = function(eleventyConfig) {
     return {
       dir: {
         input: "./", // This is relative to the config file
-        output: "../public" // Output to a folder outside of src
+        output: "../docs" // Output to a folder outside of src
       }
     };
   };
